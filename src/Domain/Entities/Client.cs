3f764cc1da4656f646e3be3cbb3c88fe97d13085
Danil Dukhovnyi <duhovny.daniil@gmail.com>
2022-10-25 12:50:09 +0300

    Redesign Client entity

--- src/Domain/Entities/Client.cs
+++ src/Domain/Entities/Client.cs
@@ -9,6 +9,8 @@ namespace HospitalRegistrationSystem.Domain.Entities
 
         public string FirstName { get; set; }
 
+        public string MiddleName { get; set; }
+
         public string LastName { get; set; }
 
         public string Gender { get; set; }
