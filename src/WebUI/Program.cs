0ad2dd9ead05e42163c79dc84391dce70ca2ed6b
Danil Dukhovnyi <duhovny.daniil@gmail.com>
2022-10-26 15:00:09 +0300

    Configure WebUI

---
+++ src/WebUI/Program.cs
@@ -0,0 +1,78 @@
+using System.IO;
+using HospitalRegistrationSystem.Application.Interfaces;
+using HospitalRegistrationSystem.Application.Mappers;
+using HospitalRegistrationSystem.WebUI.Exstensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NLog;
+
+namespace HospitalRegistrationSystem.WebUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
+                "/nlog.config"));
+
+            IServiceCollection services = builder.Services;
+            IConfiguration configuration = builder.Configuration;
+
+            services.ConfigureCors();
+            services.ConfigureLoggerService();
+
+            services.ConfigureSqlContext(configuration);
+            services.ConfigureRepositoryManager();
+
+            services.ConfigureEntityServices();
+
+            services.AddAutoMapper(typeof(MappingProfile));
+
+            services.AddControllers(config =>
+            {
+                config.RespectBrowserAcceptHeader = true;
+                config.ReturnHttpNotAcceptable = true;
+            }).AddNewtonsoftJson()
+            .AddXmlDataContractSerializerFormatters();
+
+            services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            var logger = app.Services.GetRequiredService<ILoggerManager>();
+            app.ConfigureExceptionHandler(logger);
+            app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
