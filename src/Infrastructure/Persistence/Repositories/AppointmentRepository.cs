5a9ccf1db2af7dc23a9674da3a951de5e105aba3
Danil Dukhovnyi <duhovny.daniil@gmail.com>
2024-05-29 01:59:17 +0300

    Add xml docs to AppointmentRepository

--- src/Infrastructure/Persistence/Repositories/AppointmentRepository.cs
+++ src/Infrastructure/Persistence/Repositories/AppointmentRepository.cs
@@ -9,14 +9,14 @@ using Microsoft.EntityFrameworkCore;
 namespace HospitalRegistrationSystem.Infrastructure.Persistence.Repositories;
 
 /// <summary>
-///     
+///     Represents a repository for accessing and manipulating appointments in the data layer.
 /// </summary>
 public class AppointmentRepository : RepositoryBase<Appointment>, IAppointmentRepository
 {
     /// <summary>
-    ///     
+    ///     Initializes a new instance of the <see cref="AppointmentRepository"/> class.
     /// </summary>
-    /// <param name="applicationContext"></param>
+    /// <param name="applicationContext">The application context.</param>
     public AppointmentRepository(ApplicationContext applicationContext)
         : base(applicationContext)
     {
@@ -38,8 +38,8 @@ public class AppointmentRepository : RepositoryBase<Appointment>, IAppointmentRe
     public async Task<Appointment?> GetAppointmentAsync(int id, bool trackChanges = false)
     {
         var appointment = await FindByCondition(a => a.Id == id
-                ,trackChanges
-                ,a => a.ApplicationUsers)
+                , trackChanges
+                , a => a.ApplicationUsers)
             .SingleOrDefaultAsync();
 
         return appointment;
@@ -50,8 +50,8 @@ public class AppointmentRepository : RepositoryBase<Appointment>, IAppointmentRe
     {
         var appointments = FindByCondition(a => a.ApplicationUsers.Any(u => u.Id == userId)
                 && (!isVisited.HasValue || a.IsVisited == isVisited)
-                ,trackChanges
-                ,a => a.ApplicationUsers)
+                , trackChanges
+                , a => a.ApplicationUsers)
             .OrderBy(a => a.Id);
 
         return await PagedList<Appointment>.ToPagedListAsync(appointments, paging.PageNumber, paging.PageSize);
